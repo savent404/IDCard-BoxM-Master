621800a6834ef1756bec55716d07774bc4e5b00c
廖元垲 <Savent_gate@outlook.com>
2016-10-18 21:25:06 +0800

    添加加法校验，增加宏定义：DEBUGE
    DEBUG为1，忽略箱子是否开关、忽略物品检测
    DEBUG为0，正常运行

--- Src/handle.c
+++ Src/handle.c
@@ -8,7 +8,14 @@
 uint32_t BOX_NUM = 1;
 uint32_t FULL_MASK = 0x80000000;
 static uint8_t leap[32][9] = {""};
-
+unsigned char Check_Func(unsigned char* pt) {
+	int num = 8;
+	unsigned char ans = 0;
+	
+	while (num--)
+		ans += *pt++;
+	return ans;
+}
 static uint8_t get_emtpy_bos(uint32_t box_status);
 
 /* cmp 8 Bytes */
@@ -61,14 +68,14 @@ void Handle_Init(void) {
 		FULL_MASK >>= 1;
 		FULL_MASK |= 0x80000000;
 	}
-	memset(leap, 0, sizeof(leap));
-	SoundPlay(0xE7);
-	for (i = 0; i < BOX_NUM; i++) {
-		if(!BSP_Box_Ctl(Box_ID(i))) {
-			SoundPlay(i);
-			HAL_Delay(2000);
-		}
-	}
+//	memset(leap, 0, (9*32));
+//	SoundPlay(0xE7);
+//	for (i = 0; i < BOX_NUM; i++) {
+//		if(!BSP_Box_Ctl(Box_ID(i))) {
+//			SoundPlay(i);
+//			HAL_Delay(2000);
+//		}
+//	}
 }
 
 /**
@@ -80,7 +87,7 @@ void Handle_Init(void) {
   */	uint32_t box_status;
 	uint8_t status = 0,i;
 	uint8_t recive_buff[8] = "";
-	uint8_t rec_flag[4];
+	uint8_t rec_flag[5];
 uint32_t ID_Check_Handle(void *arg) {
 
 	rec_flag[0] = getchar();
@@ -91,17 +98,33 @@ uint32_t ID_Check_Handle(void *arg) {
 	}
 	rec_flag[2] = getchar();
 	rec_flag[3] = getchar();
+	rec_flag[4] = getchar();
 	
 	/* rec_flag check */
-	if (rec_flag[0] != rec_flag[1])
+	if (rec_flag[0] != rec_flag[1]) {
+		memset(rec_flag, 0, 5);
 		return 0xFF;
-	if (rec_flag[2] != rec_flag[3])
+	}
+	if (rec_flag[2] != rec_flag[3]) {
+		memset(rec_flag, 0, 5);
 		return 0xFF;
-	if (rec_flag[0] != 0xAA)
+	}
+	if (rec_flag[0] != 0xAA) {
+		memset(rec_flag, 0, 5);
 		return 0xFF;
-	if (rec_flag[2] != 0x55)
-		return 0x55;
+	}
+	if (rec_flag[2] != 0x55) {
+		memset(rec_flag, 0, 5);
+		return 0xFF;
+	}
+	if (rec_flag[4] != Check_Func(recive_buff)) {
+		memset(rec_flag, 0, 5);
+		return 0xFF;
+	}
+	memset(rec_flag, 0, 4);
 	
+	#if (DEBUGE)
+	#else
 	/* check box status */
 	box_status = BSP_Box_Check_L();
 	if (box_status != 0xFFFFFFFF) {
@@ -110,7 +133,6 @@ uint32_t ID_Check_Handle(void *arg) {
 		/* return error */
 		return 0x01;
 	}
-	
 	/* check void box */
 	box_status = BSP_Box_Check_IF();
 	
@@ -121,13 +143,11 @@ uint32_t ID_Check_Handle(void *arg) {
 		}
 		box_status >>= 1;
 	}
-	
+	#endif
 	/* search the same id in leap */
 	status = 0;
 	for (i = 0; i < BOX_NUM; i++) {
 		if (!leap_cmp(leap[i] + 1, recive_buff)) {
-			SoundPlay(i);
-			HAL_Delay(2000);
 			status = 1;
 			break;
 		}
